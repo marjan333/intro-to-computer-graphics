59f67209fa9aa703bff738b8272f331654b3a7fb
Marjan <marjan.safvati@student.uts.edu.au>
2020-06-01 20:21:28 +1000

    updates

--- solar_system/main.js
+++ solar_system/main.js
@@ -416,6 +416,28 @@ function init() {
 
     // Start the animation.
     update(renderer, scene, camera, controls);
+
+    ///////////////////////////////////////// sound/audio //////////////////////////////////////////////
+
+// create an AudioListener and add it to the camera
+var listener = new THREE.AudioListener();
+camera.add( listener );
+
+// create a global audio source
+var sound = new THREE.Audio( listener );
+
+// load a sound and set it as the Audio object's buffer
+var audioLoader = new THREE.AudioLoader();
+audioLoader.load( 'img/space.mp3', function( buffer ) {
+	sound.setBuffer( buffer );
+	sound.setLoop( true );
+	sound.setVolume( 0.5 );
+	sound.play();
+});
+
+function touchStarted() {
+  getAudioContext().resume();
+}
 }
 
 // Start everything.
